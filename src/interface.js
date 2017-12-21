51d59b1139561825c33fb0d0b7a97535a3a6228a
Xin Wang <wangxin00163@yahoo.co.uk>
2017-12-21 18:19:27 +0000

    need to fix bug

--- src/interface.js
+++ src/interface.js
@@ -36,14 +36,22 @@ $( document ).ready(function(){
     thermostat.powerSavingOff()
     $("#power-saving-status").text(thermostat.isPowerSavingOn())
   })
+  //
+  //  $.get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=47c1b6aeef267b6a8f2431fad83b01e4", function(weatherLondon){
+  //    var temp = Math.round(weatherLondon.main.temp - 273.15) + String.fromCharCode(176) + "C"
+  //    $("#city_name").text("London Current Temperature: ")
+  //    $("#city_temp").text(temp)
+  // })
 
-   $.get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=47c1b6aeef267b6a8f2431fad83b01e4", function(weatherLondon){
-     var temp = Math.round(weatherLondon.main.temp - 273.15) + String.fromCharCode(176) + "C"
-     $("#city_name").text("London Current Temperature: ")
-     $("#city_temp").text(temp)
-  // <p>°C =&nbsp;(°F − 32) x&nbsp;5/9</p>
 
-  })
+  var city = getElementById("city_name");
+
+
+  $.get(url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'xinvale&appid=47c1b6aeef267b6a8f2431fad83b01e4', function(weatherLondon){
+    var temp = Math.round(weatherLondon.main.temp - 273.15) + String.fromCharCode(176) + "C"
+    $("#city_name").text(city + "temperature: ")
+    $("#city_temp").text(temp)
+ })
 
 
   })
