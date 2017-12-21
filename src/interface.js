4f9ba2824e3fdd10b30f3ec61406f508f9a9f26e
Valentina <valentina.rmo@gmail.com>
2017-12-21 17:38:14 +0000

    ➜  thermostat git:(master) atom . Use a jQuery AJAX call to get the weather information for London from a weather API \

--- src/interface.js
+++ src/interface.js
@@ -3,7 +3,7 @@ $( document ).ready(function(){
   var thermostat = new Thermostat()
 
   function updateTemperature(){
-    $("#current_temp").text(thermostat.gettingCurrentTemperature())
+    $("#current_temp").text(thermostat.gettingCurrentTemperature() +  String.fromCharCode(176) + "C")
     $("#current_temp").attr('class', thermostat.energyUsage())
   }
 
@@ -35,6 +35,14 @@ $( document ).ready(function(){
   $("#powersavingoff").click(function(){
     thermostat.powerSavingOff()
     $("#power-saving-status").text(thermostat.isPowerSavingOn())
+  })
+
+   $.get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=47c1b6aeef267b6a8f2431fad83b01e4", function(weatherLondon){
+     var temp = Math.round(weatherLondon.main.temp - 273.15) + String.fromCharCode(176) + "C"
+     $("#city_name").text("London Current Temperature: ")
+     $("#city_temp").text(temp)
+  // <p>°C =&nbsp;(°F − 32) x&nbsp;5/9</p>
+
   })
 
 
